0299acb121339f2bb1b429683424c056e072494c
Banupriya Subbaraman <banupriya4@gmail.com>
2025-04-23 11:32:15 -0400

    Update Day 1 Banupriya - DB_Table_Create_Copy Statements.sql

--- Day1/Day 1 Banupriya - DB_Table_Create_Copy Statements.sql
+++ Day1/Day 1 Banupriya - DB_Table_Create_Copy Statements.sql
@@ -2,11 +2,11 @@
 ---------------------------- Create Database Statement---------------------------
 ----------------------------------------------------------------------------------------
 
--- Database: NorthwindTraders
+-- Database: NorthwindTradersDB
 
--- DROP DATABASE IF EXISTS "NorthwindTraders";
+-- DROP DATABASE IF EXISTS "NorthwindTradersDB";
 
-CREATE DATABASE "NorthwindTraders"
+CREATE DATABASE "NorthwindTradersDB"
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
@@ -29,8 +29,8 @@ CREATE DATABASE "NorthwindTraders"
 CREATE TABLE IF NOT EXISTS public.categories
 (
     "categoryID" smallint NOT NULL,
-    "categoryName" character varying(100) COLLATE pg_catalog."default",
-    description character varying(255) COLLATE pg_catalog."default",
+    "categoryName" character varying(100) COLLATE pg_catalog."default" NOT NULL UNIQUE,
+    description character varying(255) COLLATE pg_catalog."default" NOT NULL UNIQUE,
     CONSTRAINT categories_pkey PRIMARY KEY ("categoryID")
 )
 
@@ -48,11 +48,11 @@ ALTER TABLE IF EXISTS public.categories
 CREATE TABLE IF NOT EXISTS public.customers
 (
     "customerID" character varying(50) NOT NULL,
-    "companyName" character varying(150) COLLATE pg_catalog."default",
-    "contactName" character varying(100) COLLATE pg_catalog."default",
-    "contactTitle" character varying(120) COLLATE pg_catalog."default",
-    city character varying(70) COLLATE pg_catalog."default",
-    country character varying(100) COLLATE pg_catalog."default",
+    "companyName" character varying(150) COLLATE pg_catalog."default" NOT NULL UNIQUE,
+    "contactName" character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    "contactTitle" character varying(120) COLLATE pg_catalog."default" NOT NULL,
+    city character varying(70) COLLATE pg_catalog."default" NOT NULL,
+    country character varying(100) COLLATE pg_catalog."default" NOT NULL,
 	CONSTRAINT customer_pkey PRIMARY KEY ("customerID")
 )
 
@@ -86,12 +86,17 @@ ALTER TABLE IF EXISTS public.datadictionary
 CREATE TABLE IF NOT EXISTS public.employees
 (
     "employeeID" smallint NOT NULL,
-    "employeeName" character varying(100) COLLATE pg_catalog."default",
-    "title" character varying(100) COLLATE pg_catalog."default",
-    city character varying(70) COLLATE pg_catalog."default",
-    country character varying(50) COLLATE pg_catalog."default",
-	reportsTo  smallint,
-	CONSTRAINT employee_pkey PRIMARY KEY ("employeeID")
+    "employeeName" character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    title character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    city character varying(70) COLLATE pg_catalog."default" NOT NULL,
+    country character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    reportsto smallint,
+    CONSTRAINT employee_pkey PRIMARY KEY ("employeeID"),
+    CONSTRAINT "employees_reportsTo_fkey" FOREIGN KEY (reportsto)
+        REFERENCES public.employees ("employeeID"),
+
+	-- Check to ensure no one reports to themselves
+    CONSTRAINT chk_no_self_reporting CHECK ("employeeID" IS DISTINCT FROM reportsto)
 )
 
 TABLESPACE pg_default;
@@ -107,7 +112,7 @@ ALTER TABLE IF EXISTS public.employees
 CREATE TABLE IF NOT EXISTS public.shippers
 (
     "shipperID" smallint NOT NULL,
-    "companyName" character varying(100) COLLATE pg_catalog."default",
+    "companyName" character varying(100) COLLATE pg_catalog."default" NOT NULL UNIQUE,
 	CONSTRAINT shipper_pkey PRIMARY KEY ("shipperID")
 )
 
@@ -126,12 +131,31 @@ CREATE TABLE IF NOT EXISTS public.orders
     "orderID" smallint NOT NULL,
     "customerID" character varying(50) NOT NULL,
     "employeeID" smallint NOT NULL,
-    orderDate date,
-    requiredDate date,
+    orderDate date NOT NULL,
+    requiredDate date NOT NULL,
 	shippedDate date,
 	"shipperID" smallint NOT NULL,
 	freight double precision NOT NULL,
 	CONSTRAINT order_pkey PRIMARY KEY ("orderID"),
+	
+	    -- Check 1: freight should not be negative
+    CONSTRAINT chk_freight_nonnegative CHECK (freight >= 0::double precision),
+
+    -- Check 2: requiredDate must be on or after orderDate
+    CONSTRAINT chk_required_after_order CHECK (
+    requiredDate >= orderDate
+    ),
+
+    -- Check 3: shippedDate must be on or after orderDate
+    CONSTRAINT chk_shipped_after_order CHECK (
+        shippedDate IS NULL OR shippedDate >= orderDate
+    ),
+
+    -- Check 4: shippedDate should be before or on requiredDate
+    --CONSTRAINT chk_shipped_before_required CHECK (
+    --    shippedDate IS NULL OR shippedDate <= requiredDate
+    --),
+	
 	CONSTRAINT order_customerid_fkey FOREIGN KEY ("customerID")
         REFERENCES public.customers ("customerID"),
 	CONSTRAINT order_employeeid_fkey FOREIGN KEY ("employeeID")
@@ -156,11 +180,14 @@ CREATE TABLE IF NOT EXISTS public.products
     productName character varying(250) NOT NULL,
     quantityPerUnit character varying(250) NOT NULL,
     unitPrice double precision NOT NULL,
-    discontinued boolean,
+    discontinued boolean NOT NULL,
 	"categoryID" smallint NOT NULL,
 	CONSTRAINT product_pkey PRIMARY KEY ("productID"),
 	CONSTRAINT product_categoryid_fkey FOREIGN KEY ("categoryID")
-        REFERENCES public.categories ("categoryID")
+        REFERENCES public.categories ("categoryID"),
+		
+	-- Check 1: unitprice should be greater than 0
+	CONSTRAINT chk_unitprice_nonzero_nonnegative CHECK (unitprice > 0::double precision) 
 )
 
 TABLESPACE pg_default;
@@ -179,12 +206,21 @@ CREATE TABLE IF NOT EXISTS public.orderdetails
     "productID" smallint NOT NULL,
     unitPrice double precision NOT NULL,
     quantity smallint NOT NULL,
-    discount double precision,
+    discount double precision NOT NULL,
     CONSTRAINT orderdetails_pkey PRIMARY KEY ("orderID", "productID"),
     CONSTRAINT orderdetails_orderid_fkey FOREIGN KEY ("orderID")
         REFERENCES public.orders ("orderID"),
     CONSTRAINT orderdetails_productid_fkey FOREIGN KEY ("productID")
-        REFERENCES public.products ("productID")
+        REFERENCES public.products ("productID"),
+		
+	-- Check 1: unitprice should be greater than 0
+	CONSTRAINT chk_unitprice_nonzero_nonnegative CHECK (unitprice > 0::double precision),
+	
+	-- Check 2: quantity should be greater than 0
+    	CONSTRAINT chk_quantity_nonzero_nonnegative CHECK (quantity > 0),
+
+	-- Check 3: discount should be between 0 and 1 (inclusive)
+	CONSTRAINT chk_discount_range CHECK (discount >= 0::double precision AND discount <= 1::double precision)
 );
 
 ALTER TABLE IF EXISTS public.orderdetails
